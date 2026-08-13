05f5a3a3e0b71c31b7f4a2af487026c6ea9e6f9f
Susheel9548 <kshusheel259@gmail.com>
2026-08-13 16:49:34 +0530

    Initial commit

---
+++ src/main/java/seekho/spring/main/Main.java
@@ -0,0 +1,27 @@
+package seekho.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import seekho.spring.model.Employee;
+
+public class Main {
+
+	public static void main(String[] args) { 
+		
+		
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		Employee bean = ioc.getBean("emp1",Employee.class);
+		
+		System.out.println(bean);
+
+
+		Employee bean2 = ioc.getBean("emp2",Employee.class);
+		
+		System.out.println(bean2);
+
+
+	}
+
+}
---
+++ src/main/java/seekho/spring/model/Employee.java
@@ -0,0 +1,56 @@
+package seekho.spring.model;
+
+public class Employee {
+	
+	private int id;
+	private String name, gender;
+	public Employee() {
+		super();
+		System.out.println("Employee.employee()");
+	
+	}
+	public Employee(int id, String name, String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		System.out.println("Employee.employee(3) ");
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		System.out.println("Employee.setId() ");
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		System.out.println("Employee.setName() ");
+		this.name = name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+		System.out.println("Employee.setGender()");
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + "]";
+	}
+	
+	
+	
+	
+
+}
